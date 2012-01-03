3434328dac5dd13d3d5249114861efdaa7327e59
duanyong <coderduan@gmail.com>
2012-01-04 00:31:02 +0800

    初始化导入

---
+++ account/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  account
+//
+//  Created by Duan Yong on 1/4/12.
+//  Copyright (c) 2012 Aiyuji.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ account/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  account
+//
+//  Created by Duan Yong on 1/4/12.
+//  Copyright (c) 2012 Aiyuji.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
